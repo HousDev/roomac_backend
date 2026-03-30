0e9e3c2ae0ded909f98caf608125fa1181c75ecc
Prachi Lad <prachi@hously.in>
2026-03-30 19:42:46 +0530

    worked on permission module

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,12 @@ router.post("/login", AuthController.login);
 
 router.get("/get-user-details/:email", AuthController.getUserDetails)
 
+router.get("/users", AuthController.getAllUsers);
+router.put("/users/:id/permissions", AuthController.updateUserPermissions);
+
+router.get("/roles", AuthController.getRoles);
+router.put("/roles/:roleName/permissions", AuthController.updateRolePermissions);
+
+router.put("/users/:id/permissions/reset", AuthController.resetUserPermissions);
+
 module.exports = router;
